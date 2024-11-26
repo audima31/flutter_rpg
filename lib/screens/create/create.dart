8c17594616e3589ff53d60ef5e46c20cbc378b77
audima31 <audima31@gmail.com>
2024-11-26 10:25:15 +0700

    selected vocation card

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/vacation.dart';
+import 'package:flutter_rpg/screens/create/vocation_card.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -10,6 +13,45 @@ class Create extends StatefulWidget {
 }
 
 class _CreateState extends State<Create> {
+  //INPUT FORM HANDLER -----------------------------------------------------------
+  //Controller untuk mengambil data input
+  final _nameController = TextEditingController();
+  final _sloganController = TextEditingController();
+
+  @override
+  //Fungsi dispose untuk memastikan agar data yang diterima akan hilang, jika sudah tidak diakses lagi, dengan tujuan untuk membuat aplikasi memiliki memeori yang efektif
+  void dispose() {
+    _nameController.dispose();
+    _sloganController.dispose();
+    super.dispose();
+  }
+
+  //Handling vocation selection
+  Vocation selectedVocation = Vocation.raider;
+
+  void updateVocation(Vocation vocation) {
+    setState(() {
+      selectedVocation = vocation;
+    });
+  }
+
+  //Submit handler
+  void handleSubmit() {
+    //trim() untuk menghapus semua spasi di awal dan akhir
+    if (_nameController.text.trim().isEmpty) {
+      print('name must not be empty');
+      return;
+    }
+
+    if (_sloganController.text.trim().isEmpty) {
+      print('slogan must not be empty');
+      return;
+    }
+    print(_nameController.text);
+    print(_sloganController.text);
+  }
+  //END INPUT FORM HANDLER -----------------------------------------------------------
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,41 +61,94 @@ class _CreateState extends State<Create> {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: Column(
-          children: [
-            Center(
-              child: Icon(
-                Icons.code,
-                color: AppColors.primaryAccent,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Icon(
+                  Icons.code,
+                  color: AppColors.primaryAccent,
+                ),
               ),
-            ),
-            const SizedBox(height: 15),
-            const Center(
-              child: StyledHeading('Welcome, new players!'),
-            ),
-            const Center(
-              child: const StyledText(
-                  'Create a name & slogan for your character. '),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
+              const SizedBox(height: 15),
+              const Center(
+                child: StyledHeading('Welcome, new players!'),
+              ),
+              const Center(
+                child:
+                    StyledText('Create a name & slogan for your character. '),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
 
-// INPUT FOR NAME AND SLOGAN -----------------
-            const TextField(
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person_2),
-                  label: StyledText('Character name')),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.chat),
-                  label: StyledText('Character slogan')),
-            )
-          ],
+              // INPUT FOR NAME AND SLOGAN -------------------------------------------------------------------------------
+              TextField(
+                controller: _nameController,
+                style: TextStyle(color: AppColors.textColor),
+                cursorColor: AppColors.primaryColor,
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person_2),
+                    label: StyledText('Character name')),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _sloganController,
+                style: TextStyle(color: AppColors.textColor),
+                cursorColor: AppColors.primaryColor,
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.chat),
+                    label: StyledText('Character slogan')),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              //Select vocation title
+              Center(
+                child: Icon(Icons.code, color: AppColors.primaryAccent),
+              ),
+
+              const SizedBox(height: 15),
+
+              const Center(
+                child: StyledHeading('Choose a Vocation'),
+              ),
+
+              const Center(
+                child: StyledText('This detemines your available skills.'),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
+
+              VocationCard(
+                  onTap: updateVocation,
+                  selected: selectedVocation == Vocation.raider,
+                  vocation: Vocation.raider),
+              VocationCard(
+                  onTap: updateVocation,
+                  selected: selectedVocation == Vocation.ninja,
+                  vocation: Vocation.ninja),
+              VocationCard(
+                  onTap: updateVocation,
+                  selected: selectedVocation == Vocation.junkie,
+                  vocation: Vocation.junkie),
+              VocationCard(
+                  onTap: updateVocation,
+                  selected: selectedVocation == Vocation.wizard,
+                  vocation: Vocation.wizard),
+              //Button Create Character----------------------------------------
+              Center(
+                child: StyledButton(
+                  onPressed: handleSubmit,
+                  child: const StyledHeading("Create Character"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/create/vocation_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/vacation.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
+
+class VocationCard extends StatelessWidget {
+  const VocationCard(
+      {super.key,
+      required this.vocation,
+      required this.onTap,
+      required this.selected});
+
+  final Vocation vocation;
+  final void Function(Vocation) onTap;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap(vocation);
+        // print(vocation.title);
+      },
+      child: Card(
+        color: selected
+            ? AppColors.primaryColor.withOpacity(0.5)
+            : Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/img/vocations/${vocation.image}',
+                width: 80,
+                //BlendMode digunakan untuk menggabungkan gambar dengan warna latar belakang
+                colorBlendMode: BlendMode.color,
+                color: !selected
+                    ? Colors.black.withOpacity(0.8)
+                    : Colors.transparent,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                  child: Column(
+                children: [
+                  StyledHeading(vocation.title),
+                  StyledText(vocation.description),
+                ],
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
